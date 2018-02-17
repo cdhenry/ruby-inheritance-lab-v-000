fbd354b319cd9884cec538023903f9e5ff1e281d
Chris Henry <cdhenry@gmail.com>
2018-02-17 19:02:04 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,18 +1,17 @@
 require_relative "./user.rb"
 
 class Student < User
-  attr_accessor :knowledge,
 
   def initialize
     @knowledge = []
   end
 
   def learn(topic)
-    self.knowledge << topic
+    @knowledge << topic
   end
 
   def knowledge
-    self.knowledge
+    @knowledge
   end
 
 end
